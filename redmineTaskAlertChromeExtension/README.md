d9ae966ee0ea28e970a946ba838fad8a738b47e4
ld <ld@tribepayments.com>
2022-03-02 12:47:14 +0200

    Redmine chrome extension final version. (non-functional).

--- redmineTaskAlertChromeExtension/README.md
+++ redmineTaskAlertChromeExtension/README.md
@@ -57,7 +57,12 @@ Communication between service worker and main client -> Broadcast channel API (e
 https://stackoverflow.com/questions/40887635/access-localstorage-from-service-worker
 
 
+Service workers remain active when clicking away from popup.
 
+
+https://www.w3.org/TR/service-workers-1/#service-worker-global-scope-fetch-event
+
+https://stackoverflow.com/questions/29741922/prevent-service-worker-from-automatically-stopping
 
 Migrating from background pages to service workers
 https://developer.chrome.com/docs/extensions/mv3/migrating_to_service_workers/
@@ -75,10 +80,51 @@ https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_worke
 Travesty Media video:
 https://www.youtube.com/watch?v=ksXwaWHCW6k&ab_channel=TraversyMedia
 
+
+Manifest 3 fields and explanations
+https://developer.chrome.com/docs/extensions/mv3/manifest/
+
+Keeping a service worker awake:
+https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension
+
+https://stackoverflow.com/questions/29741922/prevent-service-worker-from-automatically-stopping
+
 # Last session notes:
-- Importing has been postponed because it is too tedious.
-- Service workers seems to be able to successfully receive messages from the main script. -> need worker to parse the data correctly -> see if it can successfully create an alert
+- [NOTED] Importing has been postponed because it is too tedious.
+- [IN-PROGRESS] Service workers seems to be able to successfully receive messages from the main script. -> need worker to parse the data correctly -> see if it can successfully create an alert
+
+A worker is kept alive for each tab that the popup.html is opened. -> it should check if a worker is active. If active, don't spawn, if no active workers -> spawn a worker. But that means you have to open the popup at least once for the script to run. A single worker should be running continuely without the need for activation.
+
+- When re-opening inspect -> nothing happens
+- When re-opening popup   -> old service worker call is shown, but also a new one is initiated.
+
 - Why is popup js being called so many times?
 - Why are there errors in the general page console (meanwhile popup console is clear/okay)
 - Send information back to main script in order to update the localStorage
 - Final testing -> does the script keep properly functioning while popup is closed? (even when browsing websites different than Redmine)
+- Prettify CSS / clean up code
+
+
+At this point it's easier to move the extension to a web page -> all you'd have to do is keep a tab open. The code could be re-used, but it would be making API calls to MERNPy backend. 
+MERNPy would just act like a proxy, simply forwarding the requests.
+
+The only way that I know of now would be to make the service worker persist indefinitely in the background, also use a shared service worker to stop spawning multiple workers, and then see if it really works as planned. Otherwise, extensions need to be clicked on to work at all, and also clicked on to initiate a service worker.
+
+
+
+
+Keep one tab open, with one service worker running (after having clicked on the popup once).
+On popup update (entering a new Task ID / status to check) -> it should send the new data to the background.ts worker again. Otherwise, as its running, it will keep checking for old info.
+- Make sure that the service worker persists. -> upon extension refresh, the worker seems to persist indefinitely, without the need to even open the popup in the first place.
+
+The extension tab worker goes on every 5sec, and is not impacted by additional tab opened -> popup opened. Even when closed, opened again, etc.
+The popup console however, shows both, the extension original worker, and creates a new worker (2x the message printing). Is the worker alive when the popup is closed tho? Don't know. 
+
+Okay so it works. I just need to use the actual code, and make sure the worker keeps requesting updated local storage as well. No need to have the tab open, just need to check that it keeps running in the background, find the conditions that kill it.
+
+
+Service worker dies if all of the tabs that were opened when it was first started are closed. So you gotta start the extension and keep the tab open somewhere, otherwise there's a risk it will just stop at some point.
+
+
+Could the worker simply call the popup script every 5sec, and that's it? No need to handle different data types, tranfering messages between the main script and the service worker. -> nope, it service worker doesn't activate the main script, also service worker cannot raise alerts by itself.
+The features are too experimental / new, there's a lack of tutorials for this to be implemented.
--- redmineTaskAlertChromeExtension/popup.ts
+++ redmineTaskAlertChromeExtension/popup.ts
@@ -37,19 +37,19 @@ element>element	div > p	Selects all <p> elements where the parent is a <div> ele
 
 // ---------------------------------------------------------------------------------
 
+// @ts-ignore
 const broadcastChannel1 = new BroadcastChannel('channel1');
 
 broadcastChannel1.onmessage = (event) => {
   // When service worker requests for localStorage items -> send the data back
-  // if (event.data === "getLocalStorageItems") {
-    console.log("Message received by main popup script: broadcastChannel1.onmessage: " + event.data);
-    broadcastChannel1.postMessage(getAndParseLocalStorageItems()); // what is the format that is sent? Should message be parsed?
-  // }
+  console.log("Message received by main popup script: broadcastChannel1.onmessage: " + event.data);
+  checkIfTaskIsTriggered()
+  // broadcastChannel1.postMessage(getAndParseLocalStorageItems()); // what is the format that is sent? Should message be parsed?
 }
 
 // --- Config ----------------------------------------------------------------------
 
-export const redmineApiToken = "97f301157f2afdc96676e988ceb58eea2d78602c";
+const redmineApiToken = "97f301157f2afdc96676e988ceb58eea2d78602c";
 
 var fieldSchema = [
   {
@@ -213,7 +213,7 @@ before work).
 3 DONE <-- Current
 */
 
-export const getAndParseLocalStorageItems = () => {
+const getAndParseLocalStorageItems = () => {
   // Init an object to store all the items
   let localStorageItems: any = {};
   // Get an array of all the localStorage keys (i.e. task IDs)
@@ -253,7 +253,7 @@ const clearLocalStorage = () => {
   localStorage.clear();
 };
 
-export const sleep = (ms: number) => {
+const sleep = (ms: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
@@ -556,5 +556,133 @@ function deleteItemFromLocalStorage(redmineTaskNumber: string) {
 
 // ---------------------------------------------------------------------------------
 
+// // --- Send a request to Redmine every 3 minutes -----------------------------------
+
+// Extension script CORS privilege:
+// https://stackoverflow.com/questions/48615701/why-can-tampermonkeys-gm-xmlhttprequest-perform-a-cors-request
+const sendRequest = async (taskId: string) => {
+  try {
+    const redmineResponse = await fetch(
+      `https://redmine.tribepayments.com/issues/${taskId}.json`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "X-Redmine-API-Key": redmineApiToken,
+        },
+        body: null,
+      }
+    );
+    return redmineResponse;
+  } catch (error) {
+    console.log("ERROR in sendRequest func" + error);
+    return "ERROR in sendRequest func";
+  }
+};
+
+// Raise an alert via Desktop notification
+// @feature - can add text with changes what happened to the ticket
+function raiseAlert(taskId: string | number) {
+  // Source for notification standard: https://notifications.spec.whatwg.org/#using-events
+
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(`
+        Task ID: ${taskId} has triggered an alert.
+      `);
+    window
+      .open(`https://redmine.tribepayments.com/issues/${taskId}`, "_blank")
+      ?.focus();
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        // var notification = new Notification("Hi there!");
+      }
+    });
+  }
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+
+  // // Plan B:
+  // // Open Window on pop-up:
+  // window.open(`https://redmine.tribepayments.com/issues/${taskId}`, '_blank')?.focus();
+
+  // // Do not allow to close window without confirming:
+  // window.addEventListener('beforeunload', function (e) {
+  //   // Cancel the event
+  //   e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  //   // Chrome requires returnValue to be set
+  //   e.returnValue = '';
+  // });
+}
+
+const checkIfTaskIsTriggered = () => {
+  setInterval(async () => {
+    // for each item in localStorage with status triggeredInThePast == "no"
+    let localStorageItems = getAndParseLocalStorageItems();
+    for (let key in localStorageItems) {
+      if (localStorageItems[key].triggeredInThePast === "no") {
+        // console.log('Request sent to Redmine for task ID: "' + key + '"');
+        let redmineResponse = await sendRequest(key);
+        // @ts-ignore
+        // JSON.parse(redmineResponse); vs redmineResponse.json() -> Use second one. It's meant for fetch requests.
+        let responseObject = await redmineResponse.json();
+        // console.log('Redmine response -> Status: ' + responseObject.issue.status.name)
+
+        // Have to separate Status check and Custom field check because of different query
+        // (e.g. responseObject.issue.custom_fields[0].value vs responseObject.issue.status.name)
+        if (localStorageItems[key].fieldToCheck === "Status") {
+          // Check if triggered. Value must match.
+          if (
+            responseObject.issue.status.name ===
+            localStorageItems[key].valueToCheck
+          ) {
+            localStorageItems[key].triggeredInThePast = "yes";
+            localStorageItems[key].triggeredAtDate = new Date();
+            localStorage.setItem(key, JSON.stringify(localStorageItems[key]));
+            raiseAlert(key);
+          } else {
+            //
+          }
+          // Check if Field is a custom_field
+        } else {
+          for (let customField of responseObject.issue.custom_fields) {
+            if (customField.name === localStorageItems[key].fieldToCheck) {
+              if (customField.value === 'Empty') {
+                customField.value = '';
+              } else if (customField.value === 'Not empty') {
+                // @feature ???
+              }
+              if (customField.value === localStorageItems[key].valueToCheck) {
+                localStorageItems[key].triggeredInThePast = "yes";
+                localStorageItems[key].triggeredAtDate = new Date();
+                localStorage.setItem(key, JSON.stringify(localStorageItems[key]));
+                raiseAlert(key);
+              }
+            } else {
+              // console.log(`customField.name === localStorageItems[key].fieldToCheck -> ${customField.name} === ${localStorageItems[key].fieldToCheck}`);
+            }
+          }
+        }
+        await sleep(300);
+      }
+    }
+  }, 10000); // @testing - 10 seconds
+};
+
+// ---------------------------------------------------------------------------------
+
 var myWorker = new Worker('background.js');
-// Service workers logs results to popup console
+// note: service workers logs results to popup console
+// send message
+myWorker.postMessage(''); // meant for keeping the worker alive
